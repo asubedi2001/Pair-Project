6d75609f5c572d6a732b370c56b2094bc6f4bfb1
asubed6503 <asubed6503@CHHB2ZQFXV1.inst.hcpss.org>
2019-04-04 12:50:14 -0400

    Created get/set methods in Cell, created abstact class Player.java

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -1,6 +1,25 @@
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JPanel;
 
 public class BoardPanel extends JPanel{
 
-	//test
+	Cell[][] grid;
+	
+	public BoardPanel() {
+		
+	}
+	void mouseListener(MouseEvent e) {
+		
+	}
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+	public void setMode(boolean solvingMode) {
+		
+	}
+
+	
 }
--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,41 @@
+import java.awt.Graphics;
 
 public class Cell {
+
+	private boolean showButtons = false;
+	//changeable means a user can click on it as a move.
+	private boolean changeable1;
+	//changeable for player 1
+	private boolean changeable2;
+	//changeable for player2/computer
+	private int state;
+	// state :   black: 1 white: 0 none placed: -1
+	
+	
+	public Cell() {
+		
+	}
+	
+	public Cell(int state) {
+		
+	}
+	
+	public void setState(int newState) {
+		if(state > -1 && state < 2) {
+			state = newState;
+		}
+	}
+	
+	public int getState() {
+		return state;
+	}
+	
+	public void setChangeable1(boolean newChangeable) {
+		changeable1 = newChangeable;
+	}
+	
+	public void setChangeable2(boolean newChangeable) {
+		changeable2 = newChangeable;
+	}
 
 }
