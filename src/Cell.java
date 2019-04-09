0c1c00cb79634add903cede95b762f91313bae53
Ajit-Mehrotra <ajitmehrotra2009@gmail.com>
2019-04-09 12:31:08 -0400

    Added a ton of methods to cell and gamestate. GameState is not complete

--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,33 @@
 
 public class Cell {
+	private int cellState;
+	/*where 0 is empty
+		  1 is  white
+		  2 is  black*/
+	private boolean placeable;
+	Cell(){
+		cellState = 0;
+		placeable = false;
+	}
+	//the following methods are for the cellState variable
+	public int getState() {
+		return cellState;
+	}
+	public void setWhite() {
+		cellState = 1;
+	}
+	public void setBlack() {
+		cellState = 2;
+	}
+	public void setEmpty() {
+		cellState = 0;
+	}
+	
+	//the following methods is for the placeable
+	public void setPlaceable(boolean isplaceable) {
+		placeable = isplaceable;
+	}
 
 }
+
+
--- src/GameState.java
+++ src/GameState.java
@@ -1,16 +1,50 @@
 
 public class GameState {
-	private int[][] Cell; 
-	
-	GameState() {
-		Cell = new int[8][8];
-	}
-	
-	public int[][] getCellArray() {
-		return Cell;
-	}
-	// need a way to access and modify the cell array though i guess that can be done through getCellArray()
-	
+	private Cell[][] cellArray; // current state of cells on board
 
-	
+	GameState() {
+		cellArray = new Cell[8][8];
+
+	}
+
+	public Cell[][] getCellArray() {
+		return cellArray;
+		// return the reference for the array of cells
+	}
+	public void updateCellArray(int row, int col, int state){
+		if(state == 0) {
+			cellArray[row][col].setEmpty();
+		} else if(state == 1) {
+			cellArray[row][col].setWhite();
+		}else if(state == 2) {
+			cellArray[row][col].setBlack();
+		}
+
+	}
+
+	public boolean isPlaceable(Cell potentialCell) {
+		//potentialCell is the cell that the user is trying to use
+
+		//creates a new array and sets contents in that array equal to cellArray's states
+		int [][] currentBoard = new int[8][8];
+		for(int rw = 0; rw < 8; rw ++) {
+			for(int cl = 0; cl< 8; cl++) {
+				currentBoard[rw][cl] = cellArray[rw][cl].getState(); 
+			}
+		}
+
+		for(int[] a: currentBoard) {
+			// search up how to do for each loop for double array
+		}
+
+	}
+
+	//look at a column, row, or diagonal. If there is a piece that is the player's color is blocked by an opponent's color and there is no empty piece in between, placeable is true
+
+	// need a way to access and modify the cell array though i guess that can be done through getCellArray()
+
+
+
 }
+
+
