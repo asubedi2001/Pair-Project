1aa911ce243128d1768686fb505caf09d6a7efa1
asubed6503 <asubed6503@CHHB2ZQFXV1.inst.hcpss.org>
2019-04-10 12:55:37 -0400

    Fixed classes, add methods, etc.
    ALSO: added images for temp use for the board/pieces

---
+++ src/Player.java
@@ -0,0 +1,11 @@
+
+public abstract class Player {
+
+	abstract void selectCell();
+	
+	abstract void selectColor();
+	
+	abstract void player1Wins();
+	
+	abstract void player2Wins();
+}
--- src/Window.java
+++ src/Window.java
@@ -1,7 +1,11 @@
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Window {
@@ -9,22 +13,43 @@ public class Window {
 	private JFrame frame;
 	private JPanel contentPane;
 	private JPanel Menu;
+	private JLabel backgroundImg;
 
 	public Window() {
 
 
-		frame = new JFrame("Sudoku");
+		frame = new JFrame("Othello");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		contentPane = new JPanel();
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 		frame.setContentPane(contentPane);
 
 		frame.setVisible(true);
-		contentPane.setPreferredSize(new Dimension(500,500));
+		contentPane.setPreferredSize(new Dimension(1600,900));
+		
+		backgroundImg = new JLabel(new ImageIcon("img/intended GUI.png"));
+		contentPane.add(backgroundImg);
 		frame.pack();
 		frame.setResizable(false);
 
 		frame.setLocationRelativeTo(null);
+		
+		contentPane.addMouseListener(new MouseListener()
+		{
+			public void mouseClicked(MouseEvent e) {
+				//detect Cell clicked
+					//place piece
+			}
+			public void mouseEntered(MouseEvent e) {}
+			public void mouseExited(MouseEvent e) {}
+
+			public void mousePressed(MouseEvent e){		
+			}
+			public void mouseReleased(MouseEvent e) {
+				
+			}
+		});
+		
 	}
 
 }
