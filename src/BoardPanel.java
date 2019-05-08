1786f7cbe52817337304a3b7c95d798bf40243c6
Ajit_Mehrotra <ajitmehrotra2009@gmail.com>
2019-05-08 12:31:13 -0400

    Fixed & Tested the calcCellCLicked Class.
    Mathematical Algorithm was correct, but the logic was incorrect as once it found it as true, the next one would obviously be false. Therefore, the xFound/yFound variable would be set to false.

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -99,8 +99,6 @@ public class BoardPanel extends JPanel {
                 xFound = (xPix >= ((widthDifference) * a) && xPix <= ((widthDifference) * a + (widthDifference)));
                 cell.setCol(a);
             }
-
-
            // System.out.println("X Interation: " + a + "  xFound: " + xFound);
 
         }
